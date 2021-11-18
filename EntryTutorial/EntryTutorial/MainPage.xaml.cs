cc1f7a30bcd44f1e9d69b95e274f222f44c8ed7d
stszn <stas2308@inbox.ru>
2021-11-19 01:03:49 +0300

    number2

--- EntryTutorial/EntryTutorial/MainPage.xaml.cs
+++ EntryTutorial/EntryTutorial/MainPage.xaml.cs
@@ -14,5 +14,15 @@ namespace EntryTutorial
         {
             InitializeComponent();
         }
+        void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            string oldText = e.OldTextValue;
+            string newText = e.NewTextValue;
+        }
+
+        void OnEntryCompleted(object sender, EventArgs e)
+        {
+            string text = ((Entry)sender).Text;
+        }
     }
 }
